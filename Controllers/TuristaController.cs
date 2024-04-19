2a32443a9a87773f3d49468fe3df156ddcc23174
Di-Rafael <diogodevesas0101@gmail.com>
2024-04-19 16:55:59 +0100

    Projeto - Trabalho

---
+++ Controllers/TuristaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Turismo.Infrastructure.Data;
+
+
+namespace Turismo.web.Controllers
+{
+    public class TuristaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TuristaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var turistas = _db.Turistas.ToList();
+
+            return View(turistas);
+        }
+    }
+}
