44cf468fc4c964200faee44bcacd3174b779fc13
Di-Rafael <diogodevesas0101@gmail.com>
2024-05-03 17:05:48 +0100

    B

--- Controllers/TuristaController.cs
+++ Controllers/TuristaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Turismo.Domain.Entities;
 using Turismo.Infrastructure.Data;
 
 
@@ -17,6 +18,80 @@ namespace Turismo.web.Controllers
             var turistas = _db.Turistas.ToList();
 
             return View(turistas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Turista obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("Name", "The description cannot exactly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Turistas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Update(int turistaId)
+        {
+            Turista? obj = _db.Turistas.FirstOrDefault(u => u.Id == turistaId);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Turista obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Turistas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Delete(int turistaId)
+        {
+            Turista? obj = _db.Turistas.FirstOrDefault(u => u.Id == turistaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Turista obj)
+        {
+            Turista? objFromDb = _db.Turistas.FirstOrDefault(v => v.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Turistas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
         }
     }
 }
---
+++ Controllers/TuristaNumberController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Turismo.Domain.Entities;
+using Turismo.Infrastructure.Data;
+
+namespace Turismo.Web.Controllers
+{
+
+    public class TuristaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TuristaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var TuristaNumbers = _db.TuristaNumbers.ToList();
+
+            return View(TuristaNumbers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TuristaNumbers obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TuristaNumbers.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa number has been created successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["error"] = "The villa could not be created.";
+            return View(obj);
+        }
+
+        public IActionResult Update(int villaId)
+        {
+            Turista? obj = _db.Turistas.FirstOrDefault(_ => _.Id == villaId);
+
+            //Villa? obj2 = _db.Villas.Find(villaId);
+            //var VillaList = _db.Villas.Where(_ => _.Price > 50 && _.Occupancy > 0);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Turista obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Turistas.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["error"] = "The villa could not be updated.";
+            return View(obj);
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Turista? obj = _db.Turistas.FirstOrDefault(_ => _.Id == villaId);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Turista obj)
+        {
+            Turista? objFromDb = _db.Turistas.FirstOrDefault(_ => _.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Turistas.Remove(objFromDb);
+                _db.SaveChanges();
+
+                TempData["success"] = "The villa has been deleted successfully.";
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["error"] = "The villa could not be deleted.";
+            return View(obj);
+        }
+    }
+}
