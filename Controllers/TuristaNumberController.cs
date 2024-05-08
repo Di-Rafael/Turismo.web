332f11bc20e94b0588a94063fc0bec52c01113b3
Di-Rafael <diogodevesas0101@gmail.com>
2024-05-08 16:02:59 +0100

    oi

--- Controllers/TuristaNumberController.cs
+++ Controllers/TuristaNumberController.cs
@@ -1,107 +1,109 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Turismo.Domain.Entities;
 using Turismo.Infrastructure.Data;
 
-namespace Turismo.Web.Controllers
+namespace Turismo.web.Controllers
 {
+	public class TuristaNumberController : Controller
+	{
+		private readonly ApplicationDbContext _db;
 
-    public class TuristaNumberController : Controller
-    {
-        private readonly ApplicationDbContext _db;
+		public TuristaNumberController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
 
-        public TuristaNumberController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
+		public IActionResult Index()
+		{
+			var TuristaNumbers = _db.TuristaNumbers
+				.Include(u => u.Turista)
+				.ToList();
 
-        public IActionResult Index()
-        {
-            var TuristaNumbers = _db.TuristaNumbers.ToList();
+			return View(TuristaNumbers);
+		}
 
-            return View(TuristaNumbers);
-        }
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(TuristaNumbers obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.TuristaNumbers.Add(obj);
+				_db.SaveChanges();
+				TempData["success"] = "The Youtuber Records has been created successfully.";
+				return RedirectToAction(nameof(Index));
+			}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(TuristaNumbers obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.TuristaNumbers.Add(obj);
-                _db.SaveChanges();
-                TempData["success"] = "The villa number has been created successfully.";
-                return RedirectToAction(nameof(Index));
-            }
+			TempData["error"] = "The Youtuber Records could not be created.";
+			return View(obj);
+		}
+		public IActionResult Update(int villaId)
+		{
+			Turista? obj = _db.Turistas.FirstOrDefault(_ => _.Id == villaId);
 
-            TempData["error"] = "The villa could not be created.";
-            return View(obj);
-        }
+			//Villa? obj2 = _db.Villas.Find(villaId);
+			//var VillaList = _db.Villas.Where(_ => _.Price > 50 && _.Occupancy > 0);
 
-        public IActionResult Update(int villaId)
-        {
-            Turista? obj = _db.Turistas.FirstOrDefault(_ => _.Id == villaId);
+			if (obj is null)
+			{
+				return RedirectToAction("Error", "Home");
+			}
 
-            //Villa? obj2 = _db.Villas.Find(villaId);
-            //var VillaList = _db.Villas.Where(_ => _.Price > 50 && _.Occupancy > 0);
+			return View(obj);
+		}
 
-            if (obj is null)
-            {
-                return RedirectToAction("Error", "Home");
-            }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Update(Turista obj)
+		{
+			if (ModelState.IsValid && obj.Id > 0)
+			{
+				_db.Turistas.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "The villa has been updated successfully.";
+				return RedirectToAction(nameof(Index));
+			}
 
-            return View(obj);
-        }
+			TempData["error"] = "The villa could not be updated.";
+			return View(obj);
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Update(Turista obj)
-        {
-            if (ModelState.IsValid && obj.Id > 0)
-            {
-                _db.Turistas.Update(obj);
-                _db.SaveChanges();
-                TempData["success"] = "The villa has been updated successfully.";
-                return RedirectToAction(nameof(Index));
-            }
+		public IActionResult Delete(int villaId)
+		{
+			Turista? obj = _db.Turistas.FirstOrDefault(_ => _.Id == villaId);
 
-            TempData["error"] = "The villa could not be updated.";
-            return View(obj);
-        }
+			if (obj is null)
+			{
+				return RedirectToAction("Error", "Home");
+			}
 
-        public IActionResult Delete(int villaId)
-        {
-            Turista? obj = _db.Turistas.FirstOrDefault(_ => _.Id == villaId);
+			return View(obj);
+		}
 
-            if (obj is null)
-            {
-                return RedirectToAction("Error", "Home");
-            }
+		[HttpPost]
+		public IActionResult Delete(Turista obj)
+		{
+			Turista? objFromDb = _db.Turistas.FirstOrDefault(_ => _.Id == obj.Id);
 
-            return View(obj);
-        }
+			if (objFromDb is not null)
+			{
+				_db.Turistas.Remove(objFromDb);
+				_db.SaveChanges();
 
-        [HttpPost]
-        public IActionResult Delete(Turista obj)
-        {
-            Turista? objFromDb = _db.Turistas.FirstOrDefault(_ => _.Id == obj.Id);
+				TempData["success"] = "The villa has been deleted successfully.";
 
-            if (objFromDb is not null)
-            {
-                _db.Turistas.Remove(objFromDb);
-                _db.SaveChanges();
+				return RedirectToAction(nameof(Index));
+			}
 
-                TempData["success"] = "The villa has been deleted successfully.";
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            TempData["error"] = "The villa could not be deleted.";
-            return View(obj);
-        }
-    }
+			TempData["error"] = "The villa could not be deleted.";
+			return View(obj);
+		}
+	}
 }
---
+++ ViewModels/TuristaNumberVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Turismo.Domain.Entities;
+
+namespace Turismo.web.ViewModels
+{
+    public class TuristaNumberVM
+    {
+        public TuristaNumbers TuristaNumbers { get; set; } = null!;
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? TuristaList { get; set; }
+    }
+}
